b6978cb649a2ed9748dbc54736a482f846b379d2
Aline Eugenia <alineeugenia007@gmail.com>
2025-09-16 22:55:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function enviarFormulario(event) {
+    event.preventDefault();
+    const nome = document.getElementById('nomeContato').value;
+    const email = document.getElementById('emailContato').value;
+    const telefone = document.getElementById('telefoneContato').value;
+
+    const mensagem = `
+    Dados enviados com sucesso!<br>
+    Nome: ${nome}<br>
+    Email: ${email}<br>
+    Telefone: ${telefone}
+    `;
+
+    document.getElementById('mensagem-confirmacao').innerHTML = mensagem;
+}
+
+function mostrarFatoGato() {
+    fetch("https://catfact.ninja/fact")
+    .then(resposta => resposta.json())
+    .then(dados => {
+        document.getElementById('fato-gato').innerText = dados.fact;
+    })
+    .catch(erro => console.error('Error ao buscar fato do gato:',erro));
+}
+
+function  mostrarImagemCachorro() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(resposta => resposta.json())
+    .then(dados => {
+        document.getElementById('imagem-cachorro').src = dados.message;
+    })
+    .catch(erro => console.error('Error ao buscar imagem de cachorro:',erro));
+}
+mostrarImagemCachorro();
